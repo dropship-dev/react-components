3ef795e205c46c196f21f78c401627c23cecc414
nvti <tiena2cva@gmail.com>
2023-06-22 13:09:29 +0000

    Update build to output generated at 7190862

--- Button/Button.stories.d.ts
+++ Button/Button.stories.d.ts
@@ -15,4 +15,3 @@ type Story = StoryObj<typeof meta>;
 export declare const Primary: Story;
 export declare const Secondary: Story;
 export declare const Large: Story;
-export declare const Small: Story;
--- Button/Button.stories.js
+++ Button/Button.stories.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Small = exports.Large = exports.Secondary = exports.Primary = void 0;
+exports.Large = exports.Secondary = exports.Primary = void 0;
 const _1 = require(".");
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
@@ -27,12 +27,6 @@ exports.Secondary = {
 exports.Large = {
     args: {
         size: "large",
-        label: "Button",
-    },
-};
-exports.Small = {
-    args: {
-        size: "small",
         label: "Button",
     },
 };
