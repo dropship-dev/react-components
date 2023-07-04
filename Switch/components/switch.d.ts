8663443f42a1a0011d58d459deecc9f311d08456
anhpt279 <anhpt279@users.noreply.github.com>
2023-07-04 03:35:28 +0000

    Update build to output generated at c8b2f43

--- Switch/components/switch.d.ts
+++ Switch/components/switch.d.ts
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as SwitchPrimitives from "@radix-ui/react-switch";
-import "../../../styles/globals.css";
 export interface ISwichProps extends React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root> {
     danger?: boolean;
 }
