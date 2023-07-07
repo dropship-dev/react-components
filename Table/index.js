7cefe08690f278457d2dd90dab24133a16e57492
anhpt279 <anhpt279@users.noreply.github.com>
2023-07-07 06:54:29 +0000

    Update build to output generated at b36797f

--- Table/index.js
+++ Table/index.js
@@ -4,6 +4,6 @@ const jsx_runtime_1 = require("react/jsx-runtime");
 const data_table_1 = require("./components/data-table");
 function TableProduct(props) {
     const { columns, data } = props;
-    return ((0, jsx_runtime_1.jsx)("div", { className: "container mx-auto py-10", children: (0, jsx_runtime_1.jsx)(data_table_1.DataTable, { columns: columns, data: data }) }));
+    return (0, jsx_runtime_1.jsx)(data_table_1.DataTable, { columns: columns, data: data });
 }
 exports.default = TableProduct;
