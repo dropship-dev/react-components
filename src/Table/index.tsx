2b329a717f0a74c08e06cde11df5d89dfd22b4da
anhpt279 <anhpt279@gmail.com>
2023-07-07 13:53:30 +0700

    fix style table

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -4,9 +4,5 @@ import { useState } from "react";
 export default function TableProduct(props: { columns: any; data: any }) {
   const { columns, data } = props;
 
-  return (
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
-    </div>
-  );
+  return <DataTable columns={columns} data={data} />;
 }
