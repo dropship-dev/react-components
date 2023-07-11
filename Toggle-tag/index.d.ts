c5df4e78201920b31ec3871a0f000fb53bce03bc
anhpt279 <anhpt279@users.noreply.github.com>
2023-07-11 03:08:18 +0000

    Update build to output generated at 16d392a

--- Toggle-tag/index.d.ts
+++ Toggle-tag/index.d.ts
@@ -3,8 +3,9 @@ import * as LabelPrimitive from "@radix-ui/react-label";
 import { type VariantProps } from "class-variance-authority";
 export interface ILabelProps extends React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> {
     content: string;
+    onClick?: () => void;
 }
 declare const ToggleTag: React.ForwardRefExoticComponent<ILabelProps & VariantProps<(props?: ({
-    variant?: "success" | "danger" | "gradientSuccess" | "gradientdanger" | null | undefined;
+    variant?: "success" | "danger" | "gradientSuccess" | "gradientDanger" | null | undefined;
 } & import("class-variance-authority/dist/types").ClassProp) | undefined) => string> & React.RefAttributes<HTMLLabelElement>>;
 export default ToggleTag;
--- Toggle-tag/index.js
+++ Toggle-tag/index.js
@@ -29,16 +29,16 @@ const React = __importStar(require("react"));
 const LabelPrimitive = __importStar(require("@radix-ui/react-label"));
 const class_variance_authority_1 = require("class-variance-authority");
 const utils_1 = require("../lib/utils");
-const labelVariants = (0, class_variance_authority_1.cva)("peer-disabled:cursor-not-allowed peer-disabled:opacity-70 h-[48px] px-[12px] py-[14px] text-[14px] font-medium rounded-[6px]", {
+const labelVariants = (0, class_variance_authority_1.cva)("peer-disabled:cursor-not-allowed peer-disabled:opacity-70 h-[48px] px-[12px] py-[14px] text-[14px] font-medium rounded-[6px] cursor-pointer", {
     variants: {
         variant: {
             success: "bg-success-50 text-success-600",
             danger: "bg-destructive-50 text-destructive-500",
             gradientSuccess: "bg-success text-success-950",
-            gradientdanger: "bg-destructive-400 text-destructive-950",
+            gradientDanger: "bg-destructive-400 text-destructive-950",
         },
     },
 });
-const ToggleTag = React.forwardRef(({ className, content, variant, ...props }, ref) => ((0, jsx_runtime_1.jsx)(LabelPrimitive.Root, { ref: ref, className: (0, utils_1.cn)(labelVariants({ variant, className })), ...props, children: content })));
+const ToggleTag = React.forwardRef(({ className, content, variant, onClick, ...props }, ref) => ((0, jsx_runtime_1.jsx)(LabelPrimitive.Root, { ref: ref, onClick: onClick, className: (0, utils_1.cn)(labelVariants({ variant, className })), ...props, children: content })));
 ToggleTag.displayName = LabelPrimitive.Root.displayName;
 exports.default = ToggleTag;
