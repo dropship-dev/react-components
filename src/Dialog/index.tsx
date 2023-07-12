fccec4ddef3b3205ba97f8ae4d332ea3d8f6b116
anhpt279 <anhpt279@gmail.com>
2023-07-12 16:12:53 +0700

    [feat]: Alert component

---
+++ src/Dialog/index.tsx
@@ -0,0 +1,37 @@
+import { Button } from "../";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./components/diaglog";
+
+export interface DialogProps {}
+
+export default function Dialog() {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button content="open" />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
