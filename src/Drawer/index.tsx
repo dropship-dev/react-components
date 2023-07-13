174305f6a8a0c6a4b589ecdd4cbed465266cd997
anhpt279 <anhpt279@gmail.com>
2023-07-13 17:09:29 +0700

    [fix]: drawer

--- src/Drawer/index.tsx
+++ src/Drawer/index.tsx
@@ -3,27 +3,27 @@ import React from "react";
 export interface DrawerProps {
   content: React.ReactNode;
   classDrawer?: string;
-  ref?: any;
 }
-function Drawer(props: DrawerProps) {
-  const { content, classDrawer, ref } = props;
+function Drawer(props: DrawerProps, ref: React.ForwardedRef<HTMLInputElement>) {
+  const { content, classDrawer } = props;
   return (
-    <div className="drawer drawer-end">
-      <input
-        onClick={() => console.log("click 111")}
-        id="my-drawer-4"
-        type="checkbox"
-        className="drawer-toggle"
-        ref={ref}
-      />
-      <div className="drawer-content"></div>
-      <div className="drawer-side">
-        <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
-        <div className={`${classDrawer} menu p-4 w-80 h-full bg-base-200`}>
-          {content}
+    <>
+      <div className="drawer drawer-end">
+        <input
+          id="my-drawer-4"
+          type="checkbox"
+          className="drawer-toggle"
+          ref={ref}
+        />
+        <div className="drawer-content"></div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
+          <div className={`${classDrawer} menu p-4 w-80 h-full bg-base-200`}>
+            {content}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
