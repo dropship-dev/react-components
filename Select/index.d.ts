d1254caa600f1ce961de1b0be6d294a7cd62020c
anhpt279 <anhpt279@users.noreply.github.com>
2023-07-17 14:52:15 +0000

    Update build to output generated at d15667f

--- Select/index.d.ts
+++ Select/index.d.ts
@@ -1,8 +1,8 @@
 import * as React from "react";
 export default function Select(props: {
     data: {
-        id: string;
         value: string;
+        label: string;
     }[];
     placeholder?: string;
     width?: string;
