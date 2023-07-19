ca10b64fb92da8f84a23adffeae72e28a8b2a3dd
nvti <tiena2cva@gmail.com>
2023-07-19 10:28:30 +0000

    Update build to output generated at 6c81c3e

--- ProductImage/index.d.ts
+++ ProductImage/index.d.ts
@@ -4,6 +4,10 @@ import { type VariantProps } from "class-variance-authority";
 declare const ProductVariants: (props?: ({
     size?: "sm" | "md" | "lg" | "xl" | "2xl" | "xs" | "3xl" | "4xl" | null | undefined;
 } & import("class-variance-authority/dist/types").ClassProp) | undefined) => string;
+declare const AvatarVariants: (props?: ({
+    size?: "sm" | "md" | "lg" | "xl" | "2xl" | "xs" | null | undefined;
+    status?: "offline" | "online" | "instance" | null | undefined;
+} & import("class-variance-authority/dist/types").ClassProp) | undefined) => string;
 export interface ILabelProps extends React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> {
     src: string;
     alt: string;
@@ -13,8 +17,8 @@ declare const ProductImage: {
     ({ className, content, size, src, alt, onCancel, ...props }: ILabelProps & VariantProps<typeof ProductVariants>): import("react/jsx-runtime").JSX.Element;
     displayName: string | undefined;
 };
-declare const Avatar: React.ForwardRefExoticComponent<ILabelProps & VariantProps<(props?: ({
-    size?: "sm" | "md" | "lg" | "xl" | "2xl" | "xs" | null | undefined;
-    status?: "offline" | "online" | "instance" | null | undefined;
-} & import("class-variance-authority/dist/types").ClassProp) | undefined) => string> & React.RefAttributes<HTMLLabelElement>>;
+declare const Avatar: {
+    ({ className, size, src, alt, status, }: ILabelProps & VariantProps<typeof AvatarVariants>): import("react/jsx-runtime").JSX.Element;
+    displayName: string | undefined;
+};
 export { ProductImage, Avatar };
