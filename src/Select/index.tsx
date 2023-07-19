7663cdbe261e40ffd66b0607f5291c6acfd378ba
Anhpt279 <anhpt279@gmail.com>
2023-07-19 10:19:29 +0700

    fix dark mode

--- src/Select/index.tsx
+++ src/Select/index.tsx
@@ -8,6 +8,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./components/select";
+import { cva } from "class-variance-authority";
+
+import { cn } from "../lib/utils";
 import { Label } from "..";
 
 export default function Select(props: {
@@ -18,14 +21,26 @@ export default function Select(props: {
   label?: string;
   onSelected?: (value: string) => void;
   defaultValue?: string;
+  size?: "xs" | "sm" | "md";
 }) {
-  const { data, placeholder, width, icon, label, onSelected, defaultValue } =
-    props;
+  const {
+    data,
+    placeholder,
+    width,
+    icon,
+    label,
+    onSelected,
+    defaultValue,
+    size,
+  } = props;
+
   return (
     <SelectData onValueChange={onSelected} defaultValue={defaultValue}>
       {label && <Label content={label} size="textxl" frontWeight={"medium"} />}
       <SelectTrigger
-        className={`${width} border-gray-300 h-10 border-[1px] border-solid rounded-[8px] gap-[8px] flex justify-start`}
+        className={`${width} ${
+          size === "xs" ? "h-10" : size === "md" ? "h-12" : "h-11"
+        } border-gray-300 py-2 border-[1px] border-solid rounded-[8px] gap-[8px] flex justify-start focus:outline-none`}
       >
         {icon}
         <div className="w-full flex flex-row justify-between text-[14px]">
