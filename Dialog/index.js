c6fbda9220ffd76276f418edf1a55f5126d7b6c4
anhpt279 <67550342+anhpt279@users.noreply.github.com>
2023-07-20 03:51:22 +0000

    Update build to output generated at 0354b60

--- Dialog/index.js
+++ Dialog/index.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 function Dialog(props) {
     const { content, onClose, open } = props;
-    return ((0, jsx_runtime_1.jsxs)("dialog", { id: "my_modal_2", className: "modal", open: open, children: [(0, jsx_runtime_1.jsx)("form", { method: "dialog", className: "modal-box dark:bg-white", children: content }), (0, jsx_runtime_1.jsx)("form", { method: "dialog", className: "modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm", children: (0, jsx_runtime_1.jsx)("button", { onClick: () => {
+    return ((0, jsx_runtime_1.jsxs)("dialog", { id: "my_modal_2", className: "modal", open: open, children: [(0, jsx_runtime_1.jsx)("form", { method: "dialog", className: "modal-box dark:bg-white bg-white", children: content }), (0, jsx_runtime_1.jsx)("form", { method: "dialog", className: "modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm", children: (0, jsx_runtime_1.jsx)("button", { onClick: () => {
                         onClose();
                     } }) })] }));
 }
--- SkeletonLoading/components/skeleton.js
+++ SkeletonLoading/components/skeleton.js
@@ -4,6 +4,6 @@ exports.Skeleton = void 0;
 const jsx_runtime_1 = require("react/jsx-runtime");
 const utils_1 = require("../../lib/utils");
 function Skeleton({ className, ...props }) {
-    return ((0, jsx_runtime_1.jsx)("div", { className: (0, utils_1.cn)("animate-pulse rounded-md bg-muted", className), ...props }));
+    return ((0, jsx_runtime_1.jsx)("div", { className: (0, utils_1.cn)("animate-pulse bg-gray-200", className), ...props }));
 }
 exports.Skeleton = Skeleton;
