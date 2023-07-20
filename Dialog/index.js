59cc700a7c2e40ab515e4f6710c55c7df5c57d47
anhpt279 <anhpt279@users.noreply.github.com>
2023-07-20 04:46:46 +0000

    Update build to output generated at 4c7ce72

--- Dialog/index.js
+++ Dialog/index.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 function Dialog(props) {
     const { content, onClose, open } = props;
-    return ((0, jsx_runtime_1.jsxs)("dialog", { id: "my_modal_2", className: "modal", open: open, children: [(0, jsx_runtime_1.jsx)("form", { method: "dialog", className: "modal-box dark:bg-white bg-white", children: content }), (0, jsx_runtime_1.jsx)("form", { method: "dialog", className: "modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm", children: (0, jsx_runtime_1.jsx)("button", { onClick: () => {
-                        onClose();
-                    } }) })] }));
+    return ((0, jsx_runtime_1.jsxs)("dialog", { id: "my_modal_2", className: "modal", open: open, children: [(0, jsx_runtime_1.jsx)("form", { method: "dialog", className: "modal-box dark:bg-white bg-white", children: content }), (0, jsx_runtime_1.jsx)("form", { method: "dialog", className: "modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm", onClick: () => {
+                    onClose();
+                } })] }));
 }
 exports.default = Dialog;
