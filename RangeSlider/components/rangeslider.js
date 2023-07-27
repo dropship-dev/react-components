deacf15f462a12d1dfcfdf27bc5d00dea9bfc49e
anhpt279 <anhpt279@users.noreply.github.com>
2023-07-27 09:34:03 +0000

    Update build to output generated at 72dc214

--- RangeSlider/components/rangeslider.js
+++ RangeSlider/components/rangeslider.js
@@ -9,12 +9,16 @@ const react_slider_1 = __importDefault(require("react-slider"));
 const utils_1 = require("../../lib/utils");
 const RangeSlider = (_props) => {
     const isVertical = _props.orientation === "vertical";
-    return ((0, jsx_runtime_1.jsx)(react_slider_1.default, { ..._props, renderThumb: (props, state) => ((0, jsx_runtime_1.jsx)("div", { ...props, className: "h-[16px] w-[16px] rounded-full bg-primary-500" }, state.index)), renderTrack: (props, state) => {
+    return ((0, jsx_runtime_1.jsx)(react_slider_1.default, { ..._props, renderThumb: (props, state) => {
+            const prop = { ...props, key: undefined };
+            return ((0, jsx_runtime_1.jsx)("div", { ...prop, className: "h-[16px] w-[16px] rounded-full bg-primary-500" }));
+        }, renderTrack: (props, state) => {
+            const prop = { ...props, key: undefined };
             const points = Array.isArray(state.value) ? state.value.length : null;
             const isMulti = points && points > 0;
             const isLast = isMulti ? state.index === points : state.index != 0;
             const isFirst = state.index === 0;
-            return ((0, react_1.createElement)("div", { ...props, key: state.index, className: (0, utils_1.cn)((isMulti ? isFirst || isLast : isLast)
+            return ((0, react_1.createElement)("div", { ...prop, key: state.index, className: (0, utils_1.cn)((isMulti ? isFirst || isLast : isLast)
                     ? "bg-gray-300"
                     : "bg-primary-500", {
                     "h-1/4 top-1/2 -translate-y-1/2": !isVertical,
@@ -22,12 +26,13 @@ const RangeSlider = (_props) => {
                     "rounded-full": true,
                 }) }));
         }, renderMark: (props) => {
-            return ((0, jsx_runtime_1.jsx)("div", { ...props, className: (0, utils_1.cn)({
+            const prop = { ...props, key: undefined };
+            return ((0, jsx_runtime_1.jsx)("div", { ...prop, className: (0, utils_1.cn)({
                     "top-1/2 -translate-y-1/2": !isVertical,
                     "left-1/2 -translate-x-1/2": isVertical,
                     "h-1 w-1": true,
                     "rounded-full bg-red-500": true,
-                }) }, props.key));
+                }) }));
         } }, _props.value?.toString()));
 };
 exports.default = RangeSlider;
