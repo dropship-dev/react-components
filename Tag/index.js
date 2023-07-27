0185ef5b00d091bbfc814bcd5e1612cbf397a03c
anhpt279 <anhpt279@users.noreply.github.com>
2023-07-27 07:07:16 +0000

    Update build to output generated at 16cfdaf

--- Tag/index.js
+++ Tag/index.js
@@ -30,14 +30,14 @@ const LabelPrimitive = __importStar(require("@radix-ui/react-label"));
 const class_variance_authority_1 = require("class-variance-authority");
 const react_icons_1 = require("@radix-ui/react-icons");
 const utils_1 = require("../lib/utils");
-const TagVariants = (0, class_variance_authority_1.cva)("leading-[20px] font-[400] border-[1px] border-solid hover:bg-primary-25 hover:border-primary-500 active:bg-primary-100 flex flex-row gap-[8px] items-center w-fit py-[6px] px-[10px] focus:shadow-[#EAECF0] focus:shadow-[0_0_0_4px]", {
+const TagVariants = (0, class_variance_authority_1.cva)("leading-[20px] font-[400] h-fit flex flex-wrap item-center border-[1px] border-solid hover:bg-primary-25 hover:border-primary-500 active:bg-primary-100 flex flex-row gap-[8px] items-center w-fit py-[6px] px-[10px] focus:shadow-[#EAECF0] focus:shadow-[0_0_0_4px]", {
     variants: {
         size: {
-            sm: "h-[34px] text-[14px] rounded-[16px]",
-            md: "h-[44px] text-[16px] rounded-[21px]",
+            sm: "text-[14px] rounded-[16px]",
+            md: "text-[16px] rounded-[21px]",
         },
     },
 });
-const Tag = React.forwardRef(({ className, content, size, onCancel, onSelectTag, ...props }, ref) => ((0, jsx_runtime_1.jsxs)(LabelPrimitive.Root, { onClick: onSelectTag ?? undefined, ref: ref, className: (0, utils_1.cn)(TagVariants({ size, className })), ...props, children: [content, onCancel && (0, jsx_runtime_1.jsx)(react_icons_1.Cross1Icon, { onClick: onCancel })] })));
+const Tag = React.forwardRef(({ className, content, size, onCancel, onSelectTag, ...props }, ref) => ((0, jsx_runtime_1.jsx)("div", { className: "w-[200px]", children: (0, jsx_runtime_1.jsxs)(LabelPrimitive.Root, { onClick: onSelectTag ?? undefined, ref: ref, className: (0, utils_1.cn)(TagVariants({ size, className })), ...props, children: [content, onCancel && (0, jsx_runtime_1.jsx)(react_icons_1.Cross1Icon, { onClick: onCancel })] }) })));
 Tag.displayName = LabelPrimitive.Root.displayName;
 exports.default = Tag;
