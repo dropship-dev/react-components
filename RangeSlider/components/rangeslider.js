0a6f0608ac6df6d6ef024088e3bcf75daf193246
anhpt279 <67550342+anhpt279@users.noreply.github.com>
2023-07-27 08:53:26 +0000

    Update build to output generated at dbe027b

--- RangeSlider/components/rangeslider.js
+++ RangeSlider/components/rangeslider.js
@@ -3,30 +3,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = require("react");
 const jsx_runtime_1 = require("react/jsx-runtime");
 const react_slider_1 = __importDefault(require("react-slider"));
 const utils_1 = require("../../lib/utils");
 const RangeSlider = (_props) => {
     const isVertical = _props.orientation === "vertical";
-    return ((0, jsx_runtime_1.jsx)(react_slider_1.default, { ..._props, renderThumb: (props, state) => ((0, jsx_runtime_1.jsx)("div", { ...props, className: "h-[16px] w-[16px] rounded-full bg-primary-500" })), renderTrack: (props, state) => {
+    return ((0, jsx_runtime_1.jsx)(react_slider_1.default, { ..._props, renderThumb: (props, state) => ((0, jsx_runtime_1.jsx)("div", { ...props, className: "h-[16px] w-[16px] rounded-full bg-primary-500" }, props.key)), renderTrack: (props, state) => {
             const points = Array.isArray(state.value) ? state.value.length : null;
             const isMulti = points && points > 0;
             const isLast = isMulti ? state.index === points : state.index != 0;
             const isFirst = state.index === 0;
-            return ((0, jsx_runtime_1.jsx)("div", { className: (0, utils_1.cn)((isMulti ? isFirst || isLast : isLast)
+            return ((0, react_1.createElement)("div", { ...props, key: props.key, className: (0, utils_1.cn)((isMulti ? isFirst || isLast : isLast)
                     ? "bg-gray-300"
                     : "bg-primary-500", {
                     "h-1/4 top-1/2 -translate-y-1/2": !isVertical,
                     "w-1/4 left-1/2 -translate-x-1/2": isVertical,
                     "rounded-full": true,
-                }) }, props.key));
+                }) }));
         }, renderMark: (props) => {
-            return ((0, jsx_runtime_1.jsx)("div", { className: (0, utils_1.cn)({
+            return ((0, jsx_runtime_1.jsx)("div", { ...props, className: (0, utils_1.cn)({
                     "top-1/2 -translate-y-1/2": !isVertical,
                     "left-1/2 -translate-x-1/2": isVertical,
                     "h-1 w-1": true,
                     "rounded-full bg-red-500": true,
                 }) }, props.key));
-        } }, _props.value?.toString()));
+        } }));
 };
 exports.default = RangeSlider;
