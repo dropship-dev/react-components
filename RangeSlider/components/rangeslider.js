752d3e236a673c778bf6b643ca31f86e2a186e56
anhpt279 <67550342+anhpt279@users.noreply.github.com>
2023-08-08 09:57:01 +0000

    Update build to output generated at 5283d75

--- RangeSlider/components/rangeslider.js
+++ RangeSlider/components/rangeslider.js
@@ -9,14 +9,14 @@ const utils_1 = require("../../lib/utils");
 const RangeSlider = (_props) => {
     const isVertical = _props.orientation === "vertical";
     return ((0, jsx_runtime_1.jsx)(react_slider_1.default, { ..._props, renderThumb: (props, state) => {
-            const { key, ...remainProps } = props;
+            // const { key, ...remainProps } = props;
             return ((0, jsx_runtime_1.jsx)("div", { ...props, className: "h-[16px] w-[16px] rounded-full bg-primary-500" }));
         }, renderTrack: (props, state) => {
             const points = Array.isArray(state.value) ? state.value.length : null;
             const isMulti = points && points > 0;
             const isLast = isMulti ? state.index === points : state.index != 0;
             const isFirst = state.index === 0;
-            const { key, ...otherProps } = props;
+            // const { key, ...otherProps } = props;
             return ((0, jsx_runtime_1.jsx)("div", { ...props, className: (0, utils_1.cn)((isMulti ? isFirst || isLast : isLast)
                     ? "bg-gray-300"
                     : "bg-primary-500", {
@@ -25,13 +25,13 @@ const RangeSlider = (_props) => {
                     "rounded-full": true,
                 }) }));
         }, renderMark: (props) => {
-            const { key, ...remainProps } = props;
+            // const { key, ...remainProps } = props;
             return ((0, jsx_runtime_1.jsx)("div", { ...props, className: (0, utils_1.cn)({
                     "top-1/2 -translate-y-1/2": !isVertical,
                     "left-1/2 -translate-x-1/2": isVertical,
                     "h-1 w-1": true,
                     "rounded-full bg-red-500": true,
                 }) }));
-        } }, _props.value?.toString()));
+        } }));
 };
 exports.default = RangeSlider;
