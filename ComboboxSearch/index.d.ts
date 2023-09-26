e3b17001db576d93527021f14897fe67152a78ee
anhpt279 <anhpt279@users.noreply.github.com>
2023-09-26 08:33:18 +0000

    Update build to output generated at 283d9d0

--- ComboboxSearch/index.d.ts
+++ ComboboxSearch/index.d.ts
@@ -5,12 +5,13 @@ interface IComboboxProps {
         label: string | React.ReactNode;
     }[];
     placeholder?: string;
-    onSelect?: (e: string) => void;
+    onSelect?: (e: string | number) => void;
     DeleteContent?: string;
     content?: string;
     onDelete?: () => void;
-    value: string;
-    setValue: (e: string) => void;
+    value: string | number;
+    setValue: (e: string | number) => void;
+    width?: number;
 }
 export default function ComboboxDemo(props: IComboboxProps): import("react/jsx-runtime").JSX.Element;
 export {};
