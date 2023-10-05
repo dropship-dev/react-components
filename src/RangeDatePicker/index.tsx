e355e7b52e91a1fa2bd419d3c6a8df198db6370a
Anhpt279 <anhpt279@gmail.com>
2023-10-05 09:01:31 +0700

    update UI rangedate picker

--- src/RangeDatePicker/index.tsx
+++ src/RangeDatePicker/index.tsx
@@ -135,7 +135,7 @@ export default function RangeDatePicker(props: IRangeDatePicker) {
           <div
             id="date"
             className={cn(
-              "w-fit justify-start text-left font-normal px-4 py-2 rounded-[6px] border-[1px] border-gray-300 flex items-center h-11",
+              "w-fit justify-start text-left font-normal px-4 py-2 rounded-[6px] border-[1px] border-gray-300 flex items-center h-11 text-ellipsis whitespace-nowrap",
               !datePicker && "text-muted-foreground",
             )}
           >
