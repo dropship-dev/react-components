33bcffdf10660aba372b2491a01158b30da59c01
anhpt279 <anhpt279#gmail.com>
2023-10-24 09:18:17 +0700

    disableBackdropClick modal

--- src/Dialog/index.tsx
+++ src/Dialog/index.tsx
@@ -6,45 +6,75 @@ interface DialogProps {
   onClose: () => void;
   classNameContent?: string;
   styleContent?: React.CSSProperties;
+  disableBackdropClick?: boolean;
 }
 function Dialog(props: DialogProps) {
-  const { content, onClose, open, sizePopup, classNameContent, styleContent } =
-    props;
+  const {
+    content,
+    onClose,
+    open,
+    sizePopup,
+    classNameContent,
+    styleContent,
+    disableBackdropClick,
+  } = props;
   if (!sizePopup) {
     return (
-      <dialog id="my_modal_2" className="modal" open={open}>
+      <dialog
+        id={disableBackdropClick ? "my_modal_6" : "my_modal_2"}
+        className="modal"
+        open={open}
+      >
         <div
           className={`modal-box dark:bg-white bg-white ${classNameContent}`}
           style={{ zIndex: 1000, ...styleContent }}
         >
           {content}
         </div>
-        <form
-          style={{ zIndex: 900 }}
-          method="dialog"
-          className="modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm"
-          onClick={() => {
-            onClose();
-          }}
-        />
+        {!disableBackdropClick ? (
+          <form
+            style={{ zIndex: 900 }}
+            method="dialog"
+            className="modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm"
+            onClick={() => {
+              onClose();
+            }}
+          />
+        ) : (
+          <div
+            style={{ zIndex: 900 }}
+            className="modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm"
+          />
+        )}
       </dialog>
     );
   } else
     return (
-      <dialog id="my_modal_2" className="modal" open={open}>
+      <dialog
+        id={disableBackdropClick ? "my_modal_6" : "my_modal_2"}
+        className="modal"
+        open={open}
+      >
         <div
           className={`modal-box dark:bg-white bg-white w-11/12 max-w-[765px] ${classNameContent}`}
           style={{ ...styleContent }}
         >
           {content}
         </div>
-        <form
-          method="dialog"
-          className="modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm"
-          onClick={() => {
-            onClose();
-          }}
-        />
+        {!disableBackdropClick ? (
+          <form
+            method="dialog"
+            className="modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm"
+            onClick={() => {
+              onClose();
+            }}
+          />
+        ) : (
+          <div
+            style={{ zIndex: 900 }}
+            className="modal-backdrop bg-[rgba(30,35,52,0.24)] backdrop-blur-sm"
+          />
+        )}
       </dialog>
     );
 }
