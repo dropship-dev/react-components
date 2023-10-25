5b40ba12ce5efaaff6960b612610919701a4ea2f
anhpt279 <67550342+anhpt279@users.noreply.github.com>
2023-10-25 04:20:09 +0000

    Update build to output generated at 2673856

--- ComboboxSearch/index.js
+++ ComboboxSearch/index.js
@@ -34,8 +34,7 @@ function ComboboxDemo(props) {
     const { data, placeholder, onSelect, DeleteContent, onDelete, value, setValue, width, content, } = props;
     const [open, setOpen] = React.useState(false);
     const [idsSelected, setIdsSelected] = React.useState("");
-    // console.log(data.find((item) => item.value === value)?.label ?? placeholder);
-    return ((0, jsx_runtime_1.jsxs)(popover_1.Popover, { open: open, onOpenChange: setOpen, children: [(0, jsx_runtime_1.jsx)(popover_1.PopoverTrigger, { asChild: true, children: (0, jsx_runtime_1.jsxs)("div", { style: { borderRadius: "6px" }, className: `text-textMD flex items-center justify-between w-full px-3 py-[10px] border-[1px] rounded-[6px]${data.find((item) => item.value === value)?.label
+    return ((0, jsx_runtime_1.jsxs)(popover_1.Popover, { open: open, onOpenChange: setOpen, children: [(0, jsx_runtime_1.jsx)(popover_1.PopoverTrigger, { asChild: true, className: "border-gray-500", children: (0, jsx_runtime_1.jsxs)("div", { style: { borderRadius: "6px" }, className: `text-textMD flex border-gray-300 items-center justify-between w-full px-3 py-[10px] border-[1px] rounded-[6px]${data.find((item) => item.value === value)?.label
                         ? "text-gray-900"
                         : "text-gray-500"} ${open
                         ? "border-primary-500 shadow-[0_0_0_4px] shadow-[#DBDDFF]"
@@ -49,6 +48,7 @@ function ComboboxDemo(props) {
                                                     : "opacity-0") })] }, i.value))) }), value !== "" ? ((0, jsx_runtime_1.jsx)("div", { className: "pt-1 border-t-[1px] border-gray-300", children: (0, jsx_runtime_1.jsx)(command_1.CommandItem, { onSelect: () => {
                                             setValue("");
                                             setOpen(false);
+                                            setIdsSelected("");
                                             onDelete && onDelete();
                                         }, children: DeleteContent ?? "Clear selection" }) })) : null] })] }) })] }));
 }
