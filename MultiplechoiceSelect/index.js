fa06494f7eeff840d6d782c0f569d992e8b2b9a5
anhpt279 <67550342+anhpt279@users.noreply.github.com>
2023-11-28 03:58:11 +0000

    Update build to output generated at abe85c5

--- MultiplechoiceSelect/index.js
+++ MultiplechoiceSelect/index.js
@@ -48,6 +48,7 @@ function MultiplechoiceSelect(props) {
     (0, react_1.useEffect)(() => {
         callbackAllSelected && callbackAllSelected(listSelected);
     }, [listSelected]);
+    console.log(listSelected);
     return ((0, jsx_runtime_1.jsx)("div", { className: width, children: (0, jsx_runtime_1.jsxs)(popover_1.Popover, { open: open, onOpenChange: (e) => setOpen(e), children: [(0, jsx_runtime_1.jsx)(popover_1.PopoverTrigger, { asChild: true, onClick: () => setOpen(true), className: `flex justify-center items-center p-0 m-0 rounded-[6px] ${open ? "shadow-[#DBDDFF] shadow-[0_0_0_3px]" : ""}`, children: (0, jsx_runtime_1.jsx)("div", { className: "w-fit h-fit", children: (0, jsx_runtime_1.jsx)(__1.Input, { value: value, readOnly: true, className: width, small: true, iconAfter: open ? ((0, jsx_runtime_1.jsx)("svg", { xmlns: "http://www.w3.org/2000/svg", width: "20", height: "20", viewBox: "0 0 20 20", fill: "none", children: (0, jsx_runtime_1.jsx)("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M9.41205 6.91205C9.73748 6.58661 10.2651 6.58661 10.5906 6.91205L15.5906 11.912C15.916 12.2375 15.916 12.7651 15.5906 13.0906C15.2651 13.416 14.7375 13.416 14.412 13.0906L10.0013 8.67981L5.59056 13.0906C5.26512 13.416 4.73748 13.416 4.41205 13.0906C4.08661 12.7651 4.08661 12.2375 4.41205 11.912L9.41205 6.91205Z", fill: "#354053" }) })) : ((0, jsx_runtime_1.jsx)("svg", { xmlns: "http://www.w3.org/2000/svg", width: "20", height: "20", viewBox: "0 0 20 20", fill: "none", children: (0, jsx_runtime_1.jsx)("path", { fillRule: "evenodd", clipRule: "evenodd", d: "M4.41205 6.91205C4.73748 6.58661 5.26512 6.58661 5.59056 6.91205L10.0013 11.3228L14.412 6.91205C14.7375 6.58661 15.2651 6.58661 15.5906 6.91205C15.916 7.23748 15.916 7.76512 15.5906 8.09056L10.5906 13.0906C10.2651 13.416 9.73748 13.416 9.41205 13.0906L4.41205 8.09056C4.08661 7.76512 4.08661 7.23748 4.41205 6.91205Z", fill: "#354053" }) })) }) }) }), (0, jsx_runtime_1.jsxs)(popover_1.PopoverContent, { className: `px-0 pb-0 ${optionTop.length > 0 ? "pt-0" : "pt-4"} ${width} bg-white w-full min-w-[200px]`, children: [optionTop?.length > 0 &&
                             optionTop.map((item, index) => {
                                 return ((0, jsx_runtime_1.jsxs)("div", { className: "px-3 py-[10px] flex flex-row gap-3 text-textSM text-gray-900 items-center hover:bg-gray-50 cursor-pointer", onClick: () => {
@@ -76,7 +77,7 @@ function MultiplechoiceSelect(props) {
                                                 setAllSelected([...allSelected, i.value]);
                                                 setListSelected([
                                                     ...listSelected,
-                                                    { value: i.value, label: i.labelInitial ?? "" },
+                                                    { value: i.value, label: item.name ?? "" },
                                                 ]);
                                             }
                                         }, children: [onCheckedChange && ((0, jsx_runtime_1.jsx)(__1.Checkbox, { onCheckedChange: (e) => {
