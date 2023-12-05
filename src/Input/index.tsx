8d890a38919dc33f008c1200bbf08e96e16236ca
Minh Nguyen <minhktmd@gmail.com>
2023-12-05 17:03:47 +0700

    Optimize Input component and add error message

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -11,6 +11,8 @@ export interface InputProps
   iconAfter?: React.ReactNode;
   placeholder?: string;
   error?: boolean;
+  errorMessage?: string;
+  labelClassName?: string;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
@@ -25,45 +27,47 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       iconAfter,
       placeholder,
       error,
+      errorMessage,
+      labelClassName,
       ...props
     },
     ref,
   ) => {
-    const [focus, setFocus] = React.useState(false);
     return (
       <div className="gap-[6px] flex flex-col">
         {label && (
-          <p className={`text-[14px] font-medium leading-[20px] text-gray-500`}>
+          <div
+            className={`font-medium text-textSM text-gray-500 ${labelClassName}`}
+          >
             {label}
-          </p>
+          </div>
         )}
         <div
-          className={cn(
-            `bg-transparent flex justify-between items-center py-[9px] px-[12px] border border-solid ${
-              error ? "border-red-500" : "border-gray-300"
-            } rounded-[8px] ${focus && "shadow-[#DBDDFF] shadow-[0_0_0_3px]"}`,
-          )}
+          className={`bg-transparent flex justify-between items-center py-2 px-3 rounded-[8px] border border-solid focus-within:shadow-[#DBDDFF] focus-within:shadow-[0_0_0_3px]  ${
+            errorMessage || error ? "border-red-500" : "border-gray-300"
+          } `}
         >
           {iconPre}
           <input
             placeholder={placeholder}
             type={type}
-            className={cn(
-              `flex h-[26px] w-full text-[16px] font-medium text-black leading-[24px] bg-transparent focus:outline-none my-auto ${
-                iconPre ? "pl-[8px]" : ""
-              } pr-3 py-2 ring-offset-background file:bg-transparent file:text-sm file:font-semibold placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 `,
-            )}
+            className={`flex h-[26px] w-full text-[16px] font-medium text-black leading-[24px] bg-transparent focus:outline-none my-auto ${
+              iconPre ? "pl-2" : ""
+            } pr-3 py-2 ring-offset-background file:bg-transparent file:text-sm file:font-semibold placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 `}
             ref={ref}
-            onFocus={() => setFocus(true)}
-            onBlur={() => setFocus(false)}
             {...props}
           />
           {iconAfter}
         </div>
+        {errorMessage && (
+          <div className="font-normal text-textXS text-destructive-500">
+            {errorMessage}
+          </div>
+        )}
         {subLabel && (
-          <p className="text-[12px] font-normal leading-[18px] text-gray-700">
+          <div className="text-textXS font-normal text-gray-700">
             {subLabel}
-          </p>
+          </div>
         )}
       </div>
     );
