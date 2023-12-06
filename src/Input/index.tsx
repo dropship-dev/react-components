0ef48df8c66ed0585710ff7b07a36524bdfd8f9f
Minh Nguyen <minhktmd@gmail.com>
2023-12-06 16:20:30 +0700

    Update Input component

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -43,14 +43,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           </div>
         )}
         <div
-          className={`bg-transparent flex gap-2 justify-between items-center py-2 px-3 rounded-[8px] border border-gray-300 focus-within:border-primary-500 focus-within:shadow-[0_0_0_4px_#DBDDFF] ${
+          className={`bg-transparent flex gap-2 justify-between items-center h-[40px] py-[7px] px-[12px] rounded-[6px] border border-gray-300 focus-within:border-primary-500 focus-within:shadow-[0_0_0_4px_#DBDDFF] ${
             isError &&
             "border-red-500 focus-within:border-red-500 focus-within:shadow-[#FDE4E2]"
           } ${inputWrapperClassName}`}
         >
           {iconPre}
           <input
-            className={`flex h-[26px] w-full text-[16px] font-medium text-black leading-[24px] bg-transparent focus:outline-none my-auto pr-3 py-2 ring-offset-background file:bg-transparent file:text-sm file:font-semibold placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
+            className={`flex h-full w-full text-textMD font-medium text-black bg-transparent focus:outline-none my-auto ring-offset-background file:bg-transparent file:text-sm file:font-semibold placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
             ref={ref}
             {...props}
           />
@@ -64,7 +64,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           </div>
         )}
         {subLabel && (
-          <div className="text-textXS font-normal text-gray-700">
+          <div className="font-normal text-textXS text-gray-700">
             {subLabel}
           </div>
         )}
