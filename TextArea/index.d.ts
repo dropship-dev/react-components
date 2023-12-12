f39fd3b721f08790ac82aa226f850f2bee43455f
minhktmd <76050217+minhktmd@users.noreply.github.com>
2023-12-12 06:30:31 +0000

    Update build to output generated at 9af8ee1

--- TextArea/index.d.ts
+++ TextArea/index.d.ts
@@ -1,5 +1,11 @@
 import * as React from "react";
 export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+    wrapperClassName?: string;
+    label?: string;
+    labelClassName?: string;
+    error?: boolean;
+    errorMessage?: string;
+    errorMessageClassName?: string;
 }
 declare const Textarea: React.ForwardRefExoticComponent<TextareaProps & React.RefAttributes<HTMLTextAreaElement>>;
 export default Textarea;
