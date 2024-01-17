ca036df1bd390d5125b30db4f8f88c8197cab5f0
anhpt279 <67550342+anhpt279@users.noreply.github.com>
2024-01-17 09:00:47 +0000

    Update build to output generated at 7b19b79

--- Tooltip/index.js
+++ Tooltip/index.js
@@ -4,6 +4,6 @@ const jsx_runtime_1 = require("react/jsx-runtime");
 const tooltip_1 = require("./tooltip");
 function TooltipComponent(props) {
     const { children, content } = props;
-    return ((0, jsx_runtime_1.jsx)(tooltip_1.TooltipProvider, { children: (0, jsx_runtime_1.jsxs)(tooltip_1.Tooltip, { open: true, children: [(0, jsx_runtime_1.jsx)(tooltip_1.TooltipTrigger, { asChild: true, children: children }), (0, jsx_runtime_1.jsx)(tooltip_1.TooltipContent, { children: (0, jsx_runtime_1.jsx)("div", { children: content }) })] }) }));
+    return ((0, jsx_runtime_1.jsx)(tooltip_1.TooltipProvider, { children: (0, jsx_runtime_1.jsxs)(tooltip_1.Tooltip, { disableHoverableContent: true, children: [(0, jsx_runtime_1.jsx)(tooltip_1.TooltipTrigger, { asChild: true, children: children }), (0, jsx_runtime_1.jsx)(tooltip_1.TooltipContent, { children: (0, jsx_runtime_1.jsx)("div", { children: content }) })] }) }));
 }
 exports.default = TooltipComponent;
