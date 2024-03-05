7637aaa5a639d228b09dc281b48b2a8f071b72cf
Tian <67550342+anhpt279@users.noreply.github.com>
2024-03-05 11:44:18 +0700

    update dropdown menu

--- src/DropDownMenu/index.tsx
+++ src/DropDownMenu/index.tsx
@@ -8,10 +8,23 @@ import {
 export interface IDropDown {
   listAction: React.ReactNode[];
   content: string | React.ReactNode;
+  open?: boolean;
 }
 
 export default function Dropdown(props: IDropDown) {
-  const { listAction, content } = props;
+  const { listAction, content, open } = props;
+  if (open !== undefined) {
+    return (
+      <DropdownMenu open={open}>
+        <DropdownMenuTrigger>{content}</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {listAction.map((item, index) => (
+            <DropdownMenuItem key={index}>{item}</DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>{content}</DropdownMenuTrigger>
