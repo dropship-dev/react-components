00a6b87a0275a3361ea2b37b980f2b5dbc0741fe
minhktmd <minhktmd@gmail.com>
2024-03-22 03:44:54 +0000

    Update build to output generated at d30200b

--- Button/index.d.ts
+++ Button/index.d.ts
@@ -2,7 +2,7 @@ import * as React from "react";
 import { type VariantProps } from "class-variance-authority";
 declare const buttonVariantsPrimary: (props?: ({
     color?: "blue" | "destructive" | "gray" | null | undefined;
-    hierarchy?: "primary" | "secondary" | "tertiary" | "link" | null | undefined;
+    hierarchy?: "link" | "primary" | "secondary" | "tertiary" | null | undefined;
     size?: "sm" | "md" | "lg" | "xl" | "2xl" | null | undefined;
 } & import("class-variance-authority/dist/types").ClassProp) | undefined) => string;
 export interface ButtonProps extends VariantProps<typeof buttonVariantsPrimary> {
--- ProductImage/index.d.ts
+++ ProductImage/index.d.ts
@@ -2,10 +2,10 @@ import * as React from "react";
 import * as LabelPrimitive from "@radix-ui/react-label";
 import { type VariantProps } from "class-variance-authority";
 declare const ProductVariants: (props?: ({
-    size?: "sm" | "md" | "lg" | "xl" | "2xl" | "xs" | "3xl" | "4xl" | null | undefined;
+    size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | null | undefined;
 } & import("class-variance-authority/dist/types").ClassProp) | undefined) => string;
 declare const AvatarVariants: (props?: ({
-    size?: "sm" | "md" | "lg" | "xl" | "2xl" | "xs" | null | undefined;
+    size?: "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | null | undefined;
     status?: "offline" | "online" | "instance" | null | undefined;
 } & import("class-variance-authority/dist/types").ClassProp) | undefined) => string;
 export interface ILabelProps extends React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> {
