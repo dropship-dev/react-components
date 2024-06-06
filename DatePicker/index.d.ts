7b3899c0ea7c8dd0c18cb9a35985035a665516e9
duchoang145 <60808009+duchoang145@users.noreply.github.com>
2024-06-06 10:08:29 +0000

    Update build to output generated at f8f8ca8

--- DatePicker/index.d.ts
+++ DatePicker/index.d.ts
@@ -1,9 +1,12 @@
 import * as React from "react";
 import "moment-timezone";
+import { Matcher } from "react-day-picker";
 interface IDatePicker {
     date: Date | undefined;
     setDate: React.Dispatch<React.SetStateAction<Date | undefined>>;
     timezone?: string;
+    showOutsideDays?: boolean;
+    disable?: Matcher | Matcher[];
 }
 declare const DatePicker: (props: IDatePicker) => import("react/jsx-runtime").JSX.Element;
 export default DatePicker;
