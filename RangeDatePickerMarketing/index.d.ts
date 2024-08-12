2e0c1364ab72ce36eafeb312e8abf409b516441f
nvti <nvti@users.noreply.github.com>
2024-08-12 07:15:07 +0000

    Update build-production to output generated at f9ac046

--- RangeDatePickerMarketing/index.d.ts
+++ RangeDatePickerMarketing/index.d.ts
@@ -1,17 +1,11 @@
 import * as React from "react";
 import { DateRange } from "react-day-picker";
-export declare enum RangeDatePickerDefaultValues {
-    TODAY = "Today",
-    THIS_WEEK = "This week",
-    THIS_MONTH = "This month",
-    THIS_YEAR = "This year",
-    ALL_TIME = "All time"
-}
+import { RangeDatePickerDefaultValues } from "..";
 interface IRangeDatePicker {
     date: DateRange | undefined;
     setDate: React.Dispatch<React.SetStateAction<DateRange | undefined>>;
     timezone?: string;
     defaultValues?: RangeDatePickerDefaultValues;
 }
-export default function RangeDatePicker(props: IRangeDatePicker): import("react/jsx-runtime").JSX.Element;
+export default function RangeDatePickerMarketing(props: IRangeDatePicker): import("react/jsx-runtime").JSX.Element;
 export {};
--- RangeDatePickerMarketing/index.js
+++ RangeDatePickerMarketing/index.js
@@ -27,7 +27,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.RangeDatePickerDefaultValues = void 0;
 const jsx_runtime_1 = require("react/jsx-runtime");
 const lucide_react_1 = require("lucide-react");
 const React = __importStar(require("react"));
@@ -35,33 +34,25 @@ const utils_1 = require("../lib/utils");
 const moment_timezone_1 = __importDefault(require("moment-timezone"));
 const __1 = require("..");
 const popover_1 = require("../ComboBox/components/popover");
-const calendar_1 = require("./components/calendar");
-var RangeDatePickerDefaultValues;
-(function (RangeDatePickerDefaultValues) {
-    RangeDatePickerDefaultValues["TODAY"] = "Today";
-    RangeDatePickerDefaultValues["THIS_WEEK"] = "This week";
-    RangeDatePickerDefaultValues["THIS_MONTH"] = "This month";
-    RangeDatePickerDefaultValues["THIS_YEAR"] = "This year";
-    RangeDatePickerDefaultValues["ALL_TIME"] = "All time";
-})(RangeDatePickerDefaultValues || (exports.RangeDatePickerDefaultValues = RangeDatePickerDefaultValues = {}));
+const calendar_1 = require("../RangeDatePicker/components/calendar");
 const generateDateRangeFromDefaultValue = (timezoneDate, defaultValue) => {
     switch (defaultValue) {
-        case RangeDatePickerDefaultValues.TODAY:
+        case __1.RangeDatePickerDefaultValues.TODAY:
             return {
                 from: new Date((0, moment_timezone_1.default)().toDate().toLocaleString("en-US", { timeZone: timezoneDate })),
                 to: new Date((0, moment_timezone_1.default)().toDate().toLocaleString("en-US", { timeZone: timezoneDate })),
             };
-        case RangeDatePickerDefaultValues.THIS_WEEK:
+        case __1.RangeDatePickerDefaultValues.THIS_WEEK:
             return {
-                from: (0, moment_timezone_1.default)().startOf("week").toDate(),
-                to: (0, moment_timezone_1.default)().endOf("week").toDate(),
+                from: (0, moment_timezone_1.default)().startOf("week").add("days", 1).toDate(),
+                to: (0, moment_timezone_1.default)().endOf("week").add("days", 1).toDate(),
             };
-        case RangeDatePickerDefaultValues.THIS_MONTH:
+        case __1.RangeDatePickerDefaultValues.THIS_MONTH:
             return {
                 from: (0, moment_timezone_1.default)(new Date()).startOf("month").toDate(),
                 to: (0, moment_timezone_1.default)(new Date()).endOf("month").toDate(),
             };
-        case RangeDatePickerDefaultValues.THIS_YEAR:
+        case __1.RangeDatePickerDefaultValues.THIS_YEAR:
             return {
                 from: (0, moment_timezone_1.default)(new Date()).startOf("year").toDate(),
                 to: (0, moment_timezone_1.default)(new Date()).endOf("year").toDate(),
@@ -73,24 +64,14 @@ const generateDateRangeFromDefaultValue = (timezoneDate, defaultValue) => {
             };
     }
 };
-function RangeDatePicker(props) {
+function RangeDatePickerMarketing(props) {
     const { date, setDate, timezone, defaultValues } = props;
     const [firstLoad, setFirstLoad] = React.useState(true);
     const timezoneDate = timezone ?? "America/Los_Angeles";
-    // var moment = require("moment-timezone");
-    // moment.tz.setDefault(timezoneDate);
+    var moment = require("moment-timezone");
+    moment.tz.setDefault(timezoneDate);
     const [datePicker, setDatePicker] = React.useState(generateDateRangeFromDefaultValue(timezoneDate, defaultValues));
     const [open, setOpen] = React.useState(false);
-    // const convertTimezone = (timezone: string) => {
-    //   const date = new Intl.DateTimeFormat("en-GB", {
-    //     dateStyle: "full",
-    //     timeStyle: "long",
-    //     timeZone: timezone,
-    //   })
-    //     .format(new Date(moment().tz(timezone).format()))
-    //     .split(" ");
-    //   return date[date.length - 1];
-    // };
     const [valueSelected, setValueSelected] = React.useState(defaultValues ?? "");
     React.useEffect(() => {
         const dateRange = generateDateRangeFromDefaultValue(timezoneDate, defaultValues);
@@ -117,35 +98,35 @@ function RangeDatePicker(props) {
         }
     }, [datePicker, firstLoad]);
     function convertDate(date, type) {
-        return new Date(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${type === "start" ? "00:00:00" : "24:00:00"}${(0, moment_timezone_1.default)(date).tz(timezoneDate).format("Z")}`);
+        return new Date(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${type === "start" ? "00:00:00" : "23:59:59"}`);
     }
     return ((0, jsx_runtime_1.jsx)("div", { className: (0, utils_1.cn)("grid gap-2"), children: (0, jsx_runtime_1.jsxs)(popover_1.Popover, { open: open, onOpenChange: (open) => setOpen(open), children: [(0, jsx_runtime_1.jsx)(popover_1.PopoverTrigger, { children: (0, jsx_runtime_1.jsxs)("div", { id: "date", className: (0, utils_1.cn)(`w-fit justify-start text-left font-normal px-4 py-2 rounded-[6px] border-[1px] border-gray-300 flex items-center h-10 text-ellipsis whitespace-nowrap ${open ? "shadow-[#DBDDFF] shadow-[0_0_0_4px]" : ""}`, !datePicker && "text-muted-foreground"), children: [(0, jsx_runtime_1.jsx)(lucide_react_1.Calendar, { className: "mr-2 h-4 w-4" }), datePicker?.from ? (valueSelected !== "" ? (valueSelected) : datePicker.to ? ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [datePicker.from.toLocaleDateString().replaceAll("-", "/"), " -", " ", datePicker.to.toLocaleDateString().replaceAll("-", "/")] })) : ((0, jsx_runtime_1.jsx)(jsx_runtime_1.Fragment, { children: datePicker.from.toLocaleDateString() }))) : ((0, jsx_runtime_1.jsx)("span", { children: "All time" }))] }) }), (0, jsx_runtime_1.jsx)(popover_1.PopoverContent, { className: "w-auto p-0", align: "start", children: (0, jsx_runtime_1.jsxs)("div", { className: "flex rounded-[10px]", children: [(0, jsx_runtime_1.jsxs)("div", { className: "flex flex-col px-4 py-3 border-r-[1px] border-gray-100", children: [(0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("Today");
                                             setDatePicker({
-                                                from: new Date((0, moment_timezone_1.default)()
+                                                from: new Date(moment()
                                                     .toDate()
                                                     .toLocaleString("en-US", { timeZone: timezoneDate })),
-                                                to: new Date((0, moment_timezone_1.default)()
+                                                to: new Date(moment()
                                                     .toDate()
                                                     .toLocaleString("en-US", { timeZone: timezoneDate })),
                                             });
                                         }, className: "w-full text-gray-900 hover:bg-primary-25 hover:text-primary-500 px-4 py-[10px] rounded-[6px] text-textSM cursor-pointer", children: "Today" }), (0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("This week");
                                             setDatePicker({
-                                                from: (0, moment_timezone_1.default)().startOf("week").toDate(),
-                                                to: (0, moment_timezone_1.default)().endOf("week").toDate(),
+                                                from: moment().startOf("week").add("days", 1).toDate(),
+                                                to: moment().endOf("week").add("days", 1).toDate(),
                                             });
                                         }, className: "w-full text-gray-900 hover:bg-primary-25 hover:text-primary-500 px-4 py-[10px] rounded-[6px] text-textSM cursor-pointer", children: "This week" }), (0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("This month");
                                             setDatePicker({
-                                                from: (0, moment_timezone_1.default)(new Date()).startOf("month").toDate(),
-                                                to: (0, moment_timezone_1.default)(new Date()).endOf("month").toDate(),
+                                                from: moment(new Date()).startOf("month").toDate(),
+                                                to: moment(new Date()).endOf("month").toDate(),
                                             });
                                         }, className: "w-full text-gray-900 hover:bg-primary-25 hover:text-primary-500 px-4 py-[10px] rounded-[6px] text-textSM cursor-pointer", children: "This month" }), (0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("This year");
                                             setDatePicker({
-                                                from: (0, moment_timezone_1.default)(new Date()).startOf("year").toDate(),
-                                                to: (0, moment_timezone_1.default)(new Date()).endOf("year").toDate(),
+                                                from: moment(new Date()).startOf("year").toDate(),
+                                                to: moment(new Date()).endOf("year").toDate(),
                                             });
                                         }, className: "w-full text-gray-900 hover:bg-primary-25 hover:text-primary-500 px-4 py-[10px] rounded-[6px] text-textSM cursor-pointer", children: "This year" }), (0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("");
@@ -179,4 +160,4 @@ function RangeDatePicker(props) {
                                                     }
                                                 } })] })] })] }) })] }) }));
 }
-exports.default = RangeDatePicker;
+exports.default = RangeDatePickerMarketing;
