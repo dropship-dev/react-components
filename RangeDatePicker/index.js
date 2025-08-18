ab05bebed1229f71907273a2b2e65a9d36b4cb1f
duchoang145 <60808009+duchoang145@users.noreply.github.com>
2025-08-18 07:42:33 +0000

    Update build to output generated at a283c90

--- RangeDatePicker/index.js
+++ RangeDatePicker/index.js
@@ -50,22 +50,22 @@ const generateDateRangeFromDefaultValue = (timezoneDate, defaultValue) => {
         case RangeDatePickerDefaultValues.TODAY:
             return {
                 from: now.clone().startOf("day").toDate(),
-                to: now.clone().endOf("day").milliseconds(0).toDate(),
+                to: now.clone().endOf("day").toDate(),
             };
         case RangeDatePickerDefaultValues.THIS_WEEK:
             return {
                 from: now.clone().startOf("isoWeek").toDate(),
-                to: now.clone().endOf("isoWeek").milliseconds(0).toDate(), // Sun 23:59:59.000
+                to: now.clone().endOf("isoWeek").toDate(), // Sun 23:59:59.000
             };
         case RangeDatePickerDefaultValues.THIS_MONTH:
             return {
                 from: now.clone().startOf("month").toDate(),
-                to: now.clone().endOf("month").milliseconds(0).toDate(),
+                to: now.clone().endOf("month").toDate(),
             };
         case RangeDatePickerDefaultValues.THIS_YEAR:
             return {
                 from: now.clone().startOf("year").toDate(),
-                to: now.clone().endOf("year").milliseconds(0).toDate(),
+                to: now.clone().endOf("year").toDate(),
             };
         default:
             return { from: undefined, to: undefined };
@@ -106,35 +106,35 @@ function RangeDatePicker(props) {
         if (type === "start") {
             return m.startOf("day").toDate();
         }
-        return m.endOf("day").milliseconds(0).toDate();
+        return m.endOf("day").toDate();
     }
     return ((0, jsx_runtime_1.jsx)("div", { className: (0, utils_1.cn)("grid gap-2"), children: (0, jsx_runtime_1.jsxs)(popover_1.Popover, { open: open, onOpenChange: (open) => setOpen(open), children: [(0, jsx_runtime_1.jsx)(popover_1.PopoverTrigger, { children: (0, jsx_runtime_1.jsxs)("div", { id: "date", className: (0, utils_1.cn)(`w-fit justify-start text-left font-normal px-4 py-2 rounded-[6px] border-[1px] border-gray-300 flex items-center h-10 text-ellipsis whitespace-nowrap ${open ? "shadow-[#DBDDFF] shadow-[0_0_0_4px]" : ""}`, !datePicker && "text-muted-foreground"), children: [(0, jsx_runtime_1.jsx)(lucide_react_1.Calendar, { className: "mr-2 h-4 w-4" }), datePicker?.from ? (valueSelected !== "" ? (valueSelected) : datePicker.to ? ((0, jsx_runtime_1.jsxs)(jsx_runtime_1.Fragment, { children: [datePicker.from.toLocaleDateString().replaceAll("-", "/"), " -", " ", datePicker.to.toLocaleDateString().replaceAll("-", "/")] })) : ((0, jsx_runtime_1.jsx)(jsx_runtime_1.Fragment, { children: datePicker.from.toLocaleDateString() }))) : ((0, jsx_runtime_1.jsx)("span", { children: "All time" }))] }) }), (0, jsx_runtime_1.jsx)(popover_1.PopoverContent, { className: "w-auto p-0", align: "start", children: (0, jsx_runtime_1.jsxs)("div", { className: "flex rounded-[10px]", children: [(0, jsx_runtime_1.jsxs)("div", { className: "flex flex-col px-4 py-3 border-r-[1px] border-gray-100", children: [(0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("Today");
                                             const now = moment_timezone_1.default.tz(timezoneDate);
                                             setDatePicker({
                                                 from: now.clone().startOf("day").toDate(),
-                                                to: now.clone().endOf("day").milliseconds(0).toDate(),
+                                                to: now.clone().endOf("day").toDate(),
                                             });
                                         }, className: "w-full text-gray-900 hover:bg-primary-25 hover:text-primary-500 px-4 py-[10px] rounded-[6px] text-textSM cursor-pointer", children: "Today" }), (0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("This week");
                                             const now = moment_timezone_1.default.tz(timezoneDate);
                                             setDatePicker({
                                                 from: now.clone().startOf("isoWeek").toDate(),
-                                                to: now.clone().endOf("isoWeek").milliseconds(0).toDate(),
+                                                to: now.clone().endOf("isoWeek").toDate(),
                                             });
                                         }, className: "w-full text-gray-900 hover:bg-primary-25 hover:text-primary-500 px-4 py-[10px] rounded-[6px] text-textSM cursor-pointer", children: "This week" }), (0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("This month");
                                             const now = moment_timezone_1.default.tz(timezoneDate);
                                             setDatePicker({
                                                 from: now.clone().startOf("month").toDate(),
-                                                to: now.clone().endOf("month").milliseconds(0).toDate(),
+                                                to: now.clone().endOf("month").toDate(),
                                             });
                                         }, className: "w-full text-gray-900 hover:bg-primary-25 hover:text-primary-500 px-4 py-[10px] rounded-[6px] text-textSM cursor-pointer", children: "This month" }), (0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("This year");
                                             const now = moment_timezone_1.default.tz(timezoneDate);
                                             setDatePicker({
                                                 from: now.clone().startOf("year").toDate(),
-                                                to: now.clone().endOf("year").milliseconds(0).toDate(),
+                                                to: now.clone().endOf("year").toDate(),
                                             });
                                         }, className: "w-full text-gray-900 hover:bg-primary-25 hover:text-primary-500 px-4 py-[10px] rounded-[6px] text-textSM cursor-pointer", children: "This year" }), (0, jsx_runtime_1.jsx)("div", { onClick: () => {
                                             setValueSelected("");
