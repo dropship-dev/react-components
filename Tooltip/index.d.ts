ad7a7ac73cec148452f2c68d9bc40083d99e2a6c
quyetbd <quyetbd@users.noreply.github.com>
2025-10-29 04:05:21 +0000

    Update build to output generated at 7d6fecf

--- Tooltip/index.d.ts
+++ Tooltip/index.d.ts
@@ -2,6 +2,7 @@
 type Props = {
     children: React.ReactNode;
     content: string;
+    side?: "top" | "bottom" | "right" | "left";
 };
-export default function TooltipComponent(props: Props): import("react/jsx-runtime").JSX.Element;
+export default function TooltipComponentCustom(props: Props): import("react/jsx-runtime").JSX.Element;
 export * from "./tooltip";
--- Tooltip/index.js
+++ Tooltip/index.js
@@ -16,9 +16,9 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsx_runtime_1 = require("react/jsx-runtime");
 const tooltip_1 = require("./tooltip");
-function TooltipComponent(props) {
-    const { children, content } = props;
-    return ((0, jsx_runtime_1.jsx)(tooltip_1.TooltipProvider, { children: (0, jsx_runtime_1.jsxs)(tooltip_1.Tooltip, { disableHoverableContent: true, children: [(0, jsx_runtime_1.jsx)(tooltip_1.TooltipTrigger, { asChild: true, children: children }), (0, jsx_runtime_1.jsx)(tooltip_1.TooltipContent, { children: (0, jsx_runtime_1.jsx)("div", { children: content }) })] }) }));
+function TooltipComponentCustom(props) {
+    const { children, content, side } = props;
+    return ((0, jsx_runtime_1.jsx)(tooltip_1.TooltipProvider, { children: (0, jsx_runtime_1.jsxs)(tooltip_1.Tooltip, { disableHoverableContent: true, children: [(0, jsx_runtime_1.jsx)(tooltip_1.TooltipTrigger, { asChild: true, children: children }), (0, jsx_runtime_1.jsx)(tooltip_1.TooltipContent, { side: side || "top", children: (0, jsx_runtime_1.jsx)("div", { children: content }) })] }) }));
 }
-exports.default = TooltipComponent;
+exports.default = TooltipComponentCustom;
 __exportStar(require("./tooltip"), exports);
